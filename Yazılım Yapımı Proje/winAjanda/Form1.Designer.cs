0358679590aa141cc4ced6b78e9e17c1f43792fd
SametRozgar <131825966+SametRozgar@users.noreply.github.com>
2023-06-18 18:24:04 +0300

    Useful cleander app
    This procekt have lgin screen and cleander app

---
+++ Yazılım Yapımı Proje/winAjanda/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace winAjanda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstOlaylar = new System.Windows.Forms.ListBox();
+            this.mcTakvim = new System.Windows.Forms.MonthCalendar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbTur = new System.Windows.Forms.ComboBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtDetay = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstOlaylar);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(11, 10, 11, 10);
+            this.groupBox1.Size = new System.Drawing.Size(312, 271);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lstOlaylar
+            // 
+            this.lstOlaylar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstOlaylar.FormattingEnabled = true;
+            this.lstOlaylar.ItemHeight = 16;
+            this.lstOlaylar.Location = new System.Drawing.Point(11, 25);
+            this.lstOlaylar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstOlaylar.Name = "lstOlaylar";
+            this.lstOlaylar.Size = new System.Drawing.Size(290, 236);
+            this.lstOlaylar.TabIndex = 0;
+            this.lstOlaylar.SelectedIndexChanged += new System.EventHandler(this.lstOlaylar_SelectedIndexChanged);
+            // 
+            // mcTakvim
+            // 
+            this.mcTakvim.Location = new System.Drawing.Point(16, 300);
+            this.mcTakvim.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
+            this.mcTakvim.MaxSelectionCount = 31;
+            this.mcTakvim.Name = "mcTakvim";
+            this.mcTakvim.TabIndex = 1;
+            this.mcTakvim.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.mcTakvim_DateChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbTur);
+            this.groupBox2.Controls.Add(this.btnSil);
+            this.groupBox2.Controls.Add(this.btnGuncelle);
+            this.groupBox2.Controls.Add(this.btnEkle);
+            this.groupBox2.Controls.Add(this.txtDetay);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.dtpTarih);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(336, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(531, 485);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // cmbTur
+            // 
+            this.cmbTur.FormattingEnabled = true;
+            this.cmbTur.Items.AddRange(new object[] {
+            "Yıldönümü",
+            "Doğumgünü",
+            "Randevu",
+            "Kutlama",
+            "Toplantı",
+            "Özel Gün"});
+            this.cmbTur.Location = new System.Drawing.Point(131, 193);
+            this.cmbTur.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbTur.Name = "cmbTur";
+            this.cmbTur.Size = new System.Drawing.Size(265, 24);
+            this.cmbTur.TabIndex = 5;
+            this.cmbTur.SelectedIndexChanged += new System.EventHandler(this.cmbTur_SelectedIndexChanged);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(145, 226);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(79, 68);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(232, 226);
+            this.btnGuncelle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(79, 68);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Image = global::winAjanda.Properties.Resources._1455800557_Plus;
+            this.btnEkle.Location = new System.Drawing.Point(319, 226);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(79, 68);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtDetay
+            // 
+            this.txtDetay.Location = new System.Drawing.Point(131, 75);
+            this.txtDetay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDetay.Multiline = true;
+            this.txtDetay.Name = "txtDetay";
+            this.txtDetay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDetay.Size = new System.Drawing.Size(265, 110);
+            this.txtDetay.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 197);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Location = new System.Drawing.Point(131, 43);
+            this.dtpTarih.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(265, 22);
+            this.dtpTarih.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 518);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.mcTakvim);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Ajanda Uygulaması";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstOlaylar;
+        private System.Windows.Forms.MonthCalendar mcTakvim;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtDetay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpTarih;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ComboBox cmbTur;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
